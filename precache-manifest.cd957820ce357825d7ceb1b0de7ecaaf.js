184804e4b008efcf9537f23e0b42b5d74823ded9
f-deniyi <holarfemilekan049@gmail.com>
2020-08-21 03:05:55 +0100

    Updates

---
+++ precache-manifest.cd957820ce357825d7ceb1b0de7ecaaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9257e4443c7161d261bb4a32379d9579",
+    "url": "/world/index.html"
+  },
+  {
+    "revision": "4c7a27c986892d3d0c48",
+    "url": "/world/static/css/main.222a2373.chunk.css"
+  },
+  {
+    "revision": "0d8980f49b7595e7f1be",
+    "url": "/world/static/js/2.d16302a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/world/static/js/2.d16302a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c7a27c986892d3d0c48",
+    "url": "/world/static/js/main.2ebe224e.chunk.js"
+  },
+  {
+    "revision": "cfb399c76b8f65a2458d",
+    "url": "/world/static/js/runtime-main.4dbf3629.js"
+  }
+]);
